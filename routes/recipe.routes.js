8ebe1e57aa04436db6946eafe5bae7cdb775dd6b
Rita Viana <91441563+ritavianarib@users.noreply.github.com>
2022-06-17 17:02:05 +0100

    Update recipe.routes.js

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,59 +1,59 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = express.Router();
+const mongoose = require('mongoose')
+const express = require("express")
 
-const data = require("../data.json");
+const router = express.Router()
 
-const Recipe = require("../models/Recipe.model");
+const data = require('../data.json')
+const Recipe = require('../models/Recipe.model')
 
-// add recipe
-router.post("/create-recipe", async (req, res) => {
-  try {
-    const newRecipe = await Recipe.create(req.body);
 
-    return res.status(200).json(newRecipe);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
+// recipes/create-recipe
+router.post('/create-recipe', async (req, res) => {
+    try {
+        const newRecipe = await Recipe.create(req.body);
 
-// add data recipes
-router.post("/create-posts", async (req, res) => {
-  try {
-    const NewRecipes = await Recipe.insertMany(data);
+        return res.status(201).json(newRecipe)
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 
-    return res.status(200).json(NewRecipes);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
+router.post('/create-posts', async (req, res) => {
+    try {
 
-//update recipe
-router.put("/update/:title/:duration", async (req, res) => {
-  const { title, duration } = req.params;
+        const newRecipes = await Recipe.insertMany(data)
+        console.log(newRecipes, 'console.log')
 
-  try {
-    const updateRecipe = await Recipe.findOneAndUpdate(
-      { title: title },
-      { duration: Number(duration) },
-      { new: true }
-    );
-    return res.status(200).json(updateRecipe);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
+        return res.status(201).json(newRecipes)
+    } catch (error) {
+        return res.status(500).json(error);
+    }
 
-//delete recipeg
-router.delete("/delete/:title", async (req, res) => {
-  const { title } = req.params;
+})
 
-  try {
-    const deleteRecipe = await Recipe.deleteOne({ title: title });
+router.put('/update/:title/:duration', async (req, res) => {
+    const { title, duration } = req.params
+    try {
+        const updateRecipe = await Recipe.findOneAndUpdate({ title: title }, { duration: Number(duration) })
+        return res.status(200).json(updateRecipe)
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
 
-    return res.status(200).json(deleteRecipe);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
-module.exports = router;
+router.delete('/delete/:title', async (req, res) => {
+    const { title } = req.params
+
+    try {
+        const deleteRecipe = await Recipe.deleteOne({ title: title })
+
+        return res.status(200).json(deleteRecipe)
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+
+})
+
+
+
+module.exports = router
